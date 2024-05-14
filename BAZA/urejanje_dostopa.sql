3e5400fe31fb38f871bfb7ce6767a37921a43896
mihajan <miha.jan2000@gmail.com>
2024-05-14 18:32:18 +0200

    omogočil dostop do baze

---
+++ BAZA/urejanje_dostopa.sql
@@ -0,0 +1,8 @@
+-- Active: 1714029462031@@baza.fmf.uni-lj.si@5432@sem2024_mihaj@public
+GRANT ALL ON DATABASE sem2024_mihaj TO ninaj WITH GRANT OPTION;
+GRANT ALL ON SCHEMA public TO ninaj WITH GRANT OPTION;
+GRANT ALL ON ALL TABLES IN SCHEMA public TO ninaj WITH GRANT OPTION;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO ninaj WITH GRANT OPTION;
+
+GRANT CONNECT ON DATABASE sem2024_mihaj TO javnost;
+########################################################################
