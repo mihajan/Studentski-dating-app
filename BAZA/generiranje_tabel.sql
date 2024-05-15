b4095d0efdfd1d93da2716a770794d275f661fcb
mihajan <miha.jan2000@gmail.com>
2024-05-15 18:12:46 +0200

    spremenil imena in dodal Uporabnika

--- BAZA/generiranje_tabel.sql
+++ BAZA/generiranje_tabel.sql
@@ -1,8 +1,9 @@
 -- Active: 1714029462031@@baza.fmf.uni-lj.si@5432@sem2024_mihaj
-CREATE TABLE Uporabnik (
-    uporabnisko_ime TEXT PRIMARY KEY,
+CREATE TABLE Oseba (
+    emso TEXT PRIMARY KEY,
     ime TEXT NOT NULL,
-    geslo TEXT NOT NULL
+    priimek TEXT NOT NULL,
+    kontakt_ig TEXT NOT NULL
 );
 
 CREATE TABLE Vprasanje (
@@ -20,26 +21,16 @@ CREATE TABLE Mozni_odgovor (
 CREATE TABLE Odgovor (
     id SERIAL PRIMARY KEY,
     id_moznega_odgovora INT,
-    username_uporabnika TEXT,
+    emso TEXT,
     FOREIGN KEY (id_moznega_odgovora) REFERENCES Mozni_odgovor(id),
-    FOREIGN KEY (username_uporabnika) REFERENCES Uporabnik(uporabnisko_ime)
-);
-
-CREATE TABLE OdgovorDTO (
-    id SERIAL PRIMARY KEY,
-    id_moznega_odgovora INT,
-    username_uporabnika TEXT,
-    odgovor TEXT NOT NULL,
-    vprasanje TEXT NOT NULL,
-    FOREIGN KEY (id_moznega_odgovora) REFERENCES Mozni_odgovor(id),
-    FOREIGN KEY (username_uporabnika) REFERENCES Uporabnik(uporabnisko_ime)
+    FOREIGN KEY (emso) REFERENCES Oseba(emso)
 );
 
 CREATE TABLE Emotion (
-    username_uporabnika1 TEXT,
-    username_uporabnika2 TEXT,
+    emso_oseba1 TEXT,
+    emso_oseba2 TEXT,
     vrednost TEXT NOT NULL,
     CONSTRAINT check_vrednost CHECK (vrednost IN ('like', 'dislike', 'block')),
-    FOREIGN KEY (username_uporabnika1) REFERENCES Uporabnik(uporabnisko_ime),
-    FOREIGN KEY (username_uporabnika2) REFERENCES Uporabnik(uporabnisko_ime)
+    FOREIGN KEY (emso_oseba1) REFERENCES Oseba(emso),
+    FOREIGN KEY (emso_oseba2) REFERENCES Oseba(emso)
 );
--- Data/Models.py
+++ Data/Models.py
@@ -1,33 +1,34 @@
 from dataclasses import dataclass, field
-#from dataclasses_json import dataclass_json
+from dataclasses_json import dataclass_json
 
 # V tej datoteki definiramo vse podatkovne modele, ki jih bomo uporabljali v aplikaciji
 
-#@dataclass_json
+@dataclass_json
 @dataclass
-class Uporabnik:
-    uporabnisko_ime: str = field(default="")    
+class Oseba:
+    emso: str = field(default="")    
     ime: str = field(default="")
-    geslo: str = field(default="")
+    priimek: str = field(default="")
+    kontakt_ig: str = field(default="")
 
 
 
-#@dataclass_json
+@dataclass_json
 @dataclass
 class Emotion:
-    username_uporabnika1: int = field(default="")
-    username_uporabnika2: int = field(default="")
+    emso_oseba1: int = field(default="")
+    emso_oseba2: int = field(default="")
     vrednost: str = field(default="") #vrednosti bodo lahko like/dislike/block
 
 
-#@dataclass_json
+@dataclass_json
 @dataclass
 class Vprasanje:
     id: int = field(default=0)
     vprasanje: str = field(default="")
 
 
-#@dataclass_json
+@dataclass_json
 @dataclass
 class Mozni_odgovor:
     id: int = field(default=0)
@@ -35,18 +36,31 @@ class Mozni_odgovor:
     id_vprasanja: int = field(default=0)
 
 
-#@dataclass_json
+@dataclass_json
 @dataclass
 class Odgovor:
     id: int = field(default=0)
     id_moznega_odgovora: int = field(default=0)
-    username_uporabnika: int = field(default="")
+    emso: int = field(default="")
 
-#@dataclass_json
+@dataclass_json
 @dataclass
 class OdgovorDTO:
     id: int = field(default=0)
     id_moznega_odgovora: int = field(default=0)
-    username_uporabnika: int = field(default="")
-    odgovor: str = field(default="")
-    vprasanje: str = field(default="")
+    emso: int = field(default="")
+    odgovor: str = field(default="") #dodatno celo besedilo odgovora
+    vprasanje: str = field(default="") #dodatno celo besedilo vprašanja
+
+@dataclass_json
+@dataclass
+class Uporabnik:
+    username: str = field(default="")
+    role: str = field(default="")
+    password_hash: str = field(default="")
+    last_login: str = field(default="")
+
+@dataclass
+class UporabnikDto:
+    username: str = field(default="")
+    role: str = field(default="")
