de592301a0dcdefd594b99975555a0615a765f60
mihajan <miha.jan2000@gmail.com>
2024-05-15 13:13:22 +0200

    napolnil bazo z vprašanji in možnimi odgovori

---
+++ BAZA/dodajanje_vprasanj.sql
@@ -0,0 +1,70 @@
+INSERT INTO Vprasanje (vprasanje) VALUES
+('Katerega spola si?'),
+('Kateri klub izmed naštetih ti je najljubši?'),
+('Kam greš najraje na pivo/koktejl?'),
+('Kje najpogosteje ješ na bone?'),
+('Na kateri šport se najpogosteje prijaviš preko POPR-a?'),
+('Ali imaš zakupljen abonma (npr. pri Drami, Mladinskem gledališču …)'),
+('Kje bi bil tvoj sanjski prvi date?'),
+('Katera beseda najbolje opiše tvoje študentsko življenje trenutno?'),
+('Kakšna je tvoja barv las?'),
+('Kako resno razmerje iščeš?');
+
+INSERT INTO Mozni_odgovor (mozni_odgovor, id_vprasanja) VALUES
+-- Katerega spola si?
+('Moški', 1),
+('Ženska', 1),
+-- Kateri klub izmed naštetih ti je najljubši?
+('Cvetličarna', 2),
+('K4', 2),
+('Cirkus', 2),
+('Square', 2),
+('Tiffany', 2),
+-- Kam greš najraje na pivo/koktejl?
+('Foerster', 3),
+('Semafor', 3),
+('Castle', 3),
+('Nabrežje 15', 3),
+('Žmauc', 3),
+-- Kje najpogosteje ješ na bone?
+('Das ist Valter', 4),
+('Dabuda', 4),
+('McDonald''s', 4),
+('Menza FE', 4),
+('Foculus', 4),
+('Allegria', 4),
+('Meta in Bazilika', 4),
+-- Na kateri šport se najpogosteje prijaviš preko POPR-a?
+('Odbojka', 5),
+('Fitnes', 5),
+('Joga', 5),
+('Pilates', 5),
+('Nogomet', 5),
+('Košarka', 5),
+('Nič od naštetega', 5),
+-- Ali imaš zakupljen abonma (npr. pri Drami, Mladinskem gledališču …)
+('Da', 6),
+('Ne', 6),
+-- Kje bi bil tvoj sanjski prvi date?
+('NUK', 7),
+('Biljard', 7),
+('Piknik v parku', 7),
+('Atlantis', 7),
+('Večerja v Asu', 7),
+-- Katera beseda najbolje opiše tvoje študentsko življenje trenutno?
+('Kaotično', 8),
+('Organizirano', 8),
+('Brezskrbno', 8),
+('Nekje med A in B', 8),
+('Na drugem planetu', 8),
+-- Kakšna je tvoja barva las?
+('Rjava', 9),
+('Črna', 9),
+('Blond', 9),
+('Oranžna', 9),
+('Drugo', 9),
+-- Kako resno razmerje iščeš?
+('One night stand', 10),
+('Iščem bodočo Ženo/Moža', 10),
+('Poletna romanca', 10),
+('Friends with benefits', 10);
