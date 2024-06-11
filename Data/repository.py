2927cf149b40c5a55533bd269c1fc290c3bd3ed1
jankovicnina <nj9985@student.uni-lj.si>
2024-06-11 21:17:16 +0200

    Popravek za binder

--- Data/repository.py
+++ Data/repository.py
@@ -1,9 +1,11 @@
-import psycopg2, psycopg2.extensions, psycopg2.extras
+import psycopg2, psycopg2.extensions, psycopg2.extras, os
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s šumniki
 import Data.auth_public as auth
 from Data.models import Oseba, OsebaDTO, OsebafullDTO, Emotion, Vprasanje, Mozni_odgovor, Odgovor, Uporabnik, UporabnikDto
 from typing import List
 
+# Preberemo port za bazo iz okoljskih spremenljivk
+DB_PORT = os.environ.get('POSTGRES_PORT', 5432)
 
 ## V tej datoteki bomo implementirali razred Repo, ki bo vseboval metode za delo z bazo.
 
@@ -11,7 +13,7 @@ from typing import List
 class Repo:
     def __init__(self):
         # Ko ustvarimo novo instanco definiramo objekt za povezavo in cursor
-        self.conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password, port=5432)
+        self.conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password, port=DB_PORT)
         self.cur = self.conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
  
